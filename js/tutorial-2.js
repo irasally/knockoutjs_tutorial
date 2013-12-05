35d11f505b0bc9a84ad4f008c23da1546af151f2
irasally <irasally@gmail.com>
2013-12-05 14:19:39 +0900

    foreach でArrayデータを表示
    Working with Lists and Collections
    1 of 5

---
+++ js/tutorial-2.js
@@ -0,0 +1,23 @@
+function SeatReservation(name, initialMeal){
+  var self = this;
+  self.name = name;
+  self.meal = ko.observable(initialMeal);
+}
+
+function ReservationsViewModel(){
+  var self = this;
+  // Non-editable catalog data
+  self.availableMeals = [
+    { mealName: "Standard (sandwich)", price: 0 },
+    { mealName: "Premium (lobster)", price: 34.95 },
+    { mealName: "Ultimate (whole zebra)", price: 290 }
+  ];
+
+  // Editable data
+  self.seats = ko.observableArray([
+    new SeatReservation("Steve", self.availableMeals[0]),
+    new SeatReservation("John", self.availableMeals[1])
+  ]);
+}
+
+ko.applyBindings(new ReservationsViewModel());
