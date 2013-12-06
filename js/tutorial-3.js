17bfc5d9cd57b6f5cc21b77cb1b31f5dac78bb64
irasally <irasally@gmail.com>
2013-12-06 14:17:51 +0900

    選択中のメール内容を表示 XSS対策のためjsonデータをローカルで取得している事ができない このコードをwebに貼付けると動作が確認できる
    Single page applications
    3 of 5

--- js/tutorial-3.js
+++ js/tutorial-3.js
@@ -4,12 +4,20 @@ function WebmailViewModel(){
   self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
   self.chosenFolderId = ko.observable();
   self.chosenFolderData = ko.observable();
+  self.chosenMailData = ko.observable();
 
   // Behaviors
   self.goToFolder = function(folder){
     self.chosenFolderId(folder);
+    self.chosenMailData(null);
     $.get('/mail', {folder: folder}, self.chosenFolderData);
   };
+  self.goToMail = function(mail){
+    self.chosenFolderId(mail.folder);
+    self.chosenFolderData(null);
+    $.get('/mail', {mailId: mail.id}, self.chosenMailData);
+  };
+  // init
   self.goToFolder('Inbox');
 }
 ko.applyBindings(new WebmailViewModel());
